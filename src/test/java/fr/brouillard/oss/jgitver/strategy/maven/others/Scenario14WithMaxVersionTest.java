c6b170093d98c1d9a4f52c98dd90bf8349bef4b6
Dawid Jarosz <dawid.jarosz@gmail.com>
2018-11-01 13:35:07 +0100

    Use ScenarioTest as superclass in  Scenario14XXXTest's

--- src/test/java/fr/brouillard/oss/jgitver/strategy/maven/others/Scenario14WithMaxVersionTest.java
+++ src/test/java/fr/brouillard/oss/jgitver/strategy/maven/others/Scenario14WithMaxVersionTest.java
@@ -15,87 +15,28 @@
  */
 package fr.brouillard.oss.jgitver.strategy.maven.others;
 
-import static fr.brouillard.oss.jgitver.Lambdas.mute;
 import static fr.brouillard.oss.jgitver.Lambdas.unchecked;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.io.IOException;
-
-import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import fr.brouillard.oss.jgitver.GitVersionCalculator;
-import fr.brouillard.oss.jgitver.Misc;
 import fr.brouillard.oss.jgitver.Scenarios;
-import fr.brouillard.oss.jgitver.Scenarios.Scenario;
+import fr.brouillard.oss.jgitver.Strategies;
 import fr.brouillard.oss.jgitver.metadata.Metadatas;
+import fr.brouillard.oss.jgitver.strategy.ScenarioTest;
 
-public class Scenario14WithMaxVersionTest {
-    private static Scenario scenario;
-    private Repository repository;
-    private Git git;
-    private GitVersionCalculator versionCalculator;
+public class Scenario14WithMaxVersionTest extends ScenarioTest {
 
-    /**
-     * Initialiaze the whole junit class tests ; creates the git scenario.
-     */
-    @BeforeClass
-    public static void initClass() {
-        scenario = Scenarios.s14_with_merges();
-        if (Misc.isDebugMode()) {
-            System.out.println("git repository created under: " + scenario.getRepositoryLocation());
-        }
+    public Scenario14WithMaxVersionTest() {
+        super(
+                Scenarios::s14_with_merges,
+                calculator -> calculator
+                        .setStrategy(Strategies.MAVEN)
+                        .setUseMaxVersion(true)
+                        .setNonQualifierBranches("master,hotfix"));
     }
-
-    /**
-     * Cleanup the whole junit scenario ; deletes the created git repository.
-     */
-    @AfterClass
-    public static void cleanupClass() {
-        try {
-            Misc.deleteDirectorySimple(scenario.getRepositoryLocation());
-        } catch (Exception ignore) {
-            System.err.println("cannot remove " + scenario.getRepositoryLocation());
-        }
-    }
-
-    /**
-     * Prepare common variables to access the git repository.
-     *
-     * @throws IOException if a disk error occurred
-     */
-    @Before
-    public void init() throws IOException {
-        repository = new FileRepositoryBuilder().setGitDir(scenario.getRepositoryLocation()).build();
-        git = new Git(repository);
-        versionCalculator = GitVersionCalculator
-                .location(scenario.getRepositoryLocation())
-                .setNonQualifierBranches("master,hotfix")
-                .setUseMaxVersion(true)
-                .setMavenLike(true);
-
-        // reset the head to master
-        unchecked(() -> git.checkout().setName("master").call());
-    }
-
-    /**
-     * Cleanups after each tests.
-     */
-    @After
-    public void clean() {
-        mute(() -> git.close());
-        mute(() -> repository.close());
-        mute(() -> versionCalculator.close());
-    }
-
 
     @Test
     public void version_of_B_commit() {
