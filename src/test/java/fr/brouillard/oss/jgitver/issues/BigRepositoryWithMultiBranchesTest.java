d21a2204cfd8bcafdbd9e7abf487a16afe4186a3
Matthieu Brouillard <matthieu@brouillard.fr>
2018-11-04 11:31:41 +0100

    provide test case for #54

---
+++ src/test/java/fr/brouillard/oss/jgitver/issues/BigRepositoryWithMultiBranchesTest.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2016 Matthieu Brouillard [http://oss.brouillard.fr/jgitver] (matthieu@brouillard.fr)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.brouillard.oss.jgitver.issues;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import fr.brouillard.oss.jgitver.ScenarioTest;
+import fr.brouillard.oss.jgitver.Scenarios;
+import fr.brouillard.oss.jgitver.Strategies;
+
+public class BigRepositoryWithMultiBranchesTest extends ScenarioTest {
+    private static int NB_COMMITS_PER_BRANCH = 2500;
+
+    /**
+     * Initialize the whole junit class tests ; creates the git scenario.
+     */
+    public BigRepositoryWithMultiBranchesTest() {
+        super(
+                BigRepositoryWithMultiBranchesTest::buildScenario,
+                calc -> calc.setStrategy(Strategies.CONFIGURABLE)
+                            .setAutoIncrementPatch(true)
+                            .setUseDistance(true)
+        );
+    }
+
+    static Scenarios.Scenario buildScenario() {
+        long start = System.currentTimeMillis();
+        try {
+            Scenarios.ScenarioBuilder builder = new Scenarios.ScenarioBuilder();
+            builder.commit("start", "A");
+            builder.tag("1.0.0");
+
+            for (int i = 0; i < NB_COMMITS_PER_BRANCH; i++) {
+                builder.commit("commit " + i, "A" + i);
+            }
+
+            builder.branchOnAppId("branchB", "A");
+            builder.commit("start of branch B", "B");
+            builder.tag("2.0.0");
+
+            for (int i = 0; i < NB_COMMITS_PER_BRANCH; i++) {
+                builder.commit("commit on branch B" + i, "B" + i);
+            }
+            builder.commit("last B", "BL");
+
+            builder.master();
+            builder.merge("BL", "C");
+
+            return builder
+                    .getScenario();
+        } finally {
+            long end = System.currentTimeMillis();
+            System.out.printf("building big scenario %s took %dms%n", BigRepositoryWithMultiBranchesTest.class.getSimpleName(), end - start);
+        }
+    }
+
+    @Test
+    public void can_compute_version() {
+        int expectedDistance = NB_COMMITS_PER_BRANCH
+                                + 1         // last commit in branch B
+                                + 1;         // merge commit;
+        assertThat(versionCalculator.getVersion(), is("2.0.1-" + expectedDistance));
+    }
+}
---
+++ src/test/java/fr/brouillard/oss/jgitver/issues/NoTagsBigRepositoryTest.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (C) 2016 Matthieu Brouillard [http://oss.brouillard.fr/jgitver] (matthieu@brouillard.fr)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.brouillard.oss.jgitver.issues;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import org.junit.Test;
+
+import fr.brouillard.oss.jgitver.GitVersionCalculator;
+import fr.brouillard.oss.jgitver.ScenarioTest;
+import fr.brouillard.oss.jgitver.Scenarios;
+import fr.brouillard.oss.jgitver.Strategies;
+
+public class NoTagsBigRepositoryTest extends ScenarioTest {
+    private static int NB_COMMITS = 2500;
+
+    /**
+     * Initialize the whole junit class tests ; creates the git scenario.
+     */
+    public NoTagsBigRepositoryTest() {
+        super(
+                NoTagsBigRepositoryTest::buildScenario,
+                calc -> calc.setStrategy(Strategies.CONFIGURABLE)
+                            .setUseDistance(true)
+        );
+    }
+
+    static Scenarios.Scenario buildScenario() {
+        long start = System.currentTimeMillis();
+        try {
+            Scenarios.ScenarioBuilder builder = new Scenarios.ScenarioBuilder();
+            for (int i = 0; i < NB_COMMITS; i++) {
+                builder.commit("commit " + i, "A" + i);
+            }
+
+            return builder
+                    .commit("last commit", "A")
+                    .master()
+                    .getScenario();
+        } finally {
+            long end = System.currentTimeMillis();
+            System.out.printf("building big scenario with %d commits took %dms%n", NB_COMMITS, end - start);
+        }
+    }
+
+    @Test
+    public void can_compute_version() {
+        assertThat(versionCalculator.getVersion(), is("0.0.0-" + NB_COMMITS));
+    }
+}
